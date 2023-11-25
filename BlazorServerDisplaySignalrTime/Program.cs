78888a77bf5929804f74b5ae761032eed9f94c90
Victor Looren de Jong <contact@victorloorendejong.nl>
2023-11-25 16:52:48 +0100

    Removed SignalR from program.cs

--- BlazorServerDisplaySignalrTime/Program.cs
+++ BlazorServerDisplaySignalrTime/Program.cs
@@ -2,42 +2,25 @@ using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.SignalR.Client;
 
-try
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
 {
-    var builder = WebApplication.CreateBuilder(args);
-    builder.Services.AddRazorPages();
-    builder.Services.AddServerSideBlazor();
-
-    // Create a SignalR connection to the specified hub
-    var connection = new HubConnectionBuilder()
-        .WithUrl("http://localhost:5104/hubs/clock")
-        .WithAutomaticReconnect()
-        .Build();
-
-    // Register the SignalR connection as a singleton service
-    builder.Services.AddSingleton(connection);
-
-    var app = builder.Build();
-
-    if (!app.Environment.IsDevelopment())
-    {
-        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-        app.UseHsts();
-    }
-
-    app.UseHttpsRedirection();
-
-    app.UseStaticFiles();
-
-    app.UseRouting();
-
-    app.MapBlazorHub();
-    app.MapFallbackToPage("/_Host");
-
-    app.Run();
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
-catch (Exception ex)
-{
-    Console.WriteLine($"An error occurred: {ex.Message}");
-    // Consider logging the error here
-}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
