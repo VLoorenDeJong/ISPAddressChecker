797ce218717ce5f0fbb11af8f7ac0e81a6b06530
Victor Looren de Jong <contact@victorloorendejong.nl>
2023-11-25 16:42:06 +0100

    SignalRClockFunctional:)

--- BlazorServerDisplaySignalrTime/Program.cs
+++ BlazorServerDisplaySignalrTime/Program.cs
@@ -1,25 +1,43 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.SignalR.Client;
 
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-
-var app = builder.Build();
-
-if (!app.Environment.IsDevelopment())
+try
 {
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+
+    // Create a SignalR connection to the specified hub
+    var connection = new HubConnectionBuilder()
+        .WithUrl("http://localhost:5104/hubs/clock")
+        .WithAutomaticReconnect()
+        .Build();
+
+    // Register the SignalR connection as a singleton service
+    builder.Services.AddSingleton(connection);
+
+    var app = builder.Build();
+
+    if (!app.Environment.IsDevelopment())
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-app.Run();
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+    // Consider logging the error here
+}
